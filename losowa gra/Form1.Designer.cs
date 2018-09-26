ac434647762d4d0ff3eb9b5ed74f56ed7f5349dc
Victorskiee <krasinski.wiktor00@gmail.com>
2018-09-26 20:49:01 +0200

    liczby losowe

---
+++ losowa gra/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace losowa_gra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(120, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Twój wynik:";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(282, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            this.label2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::losowa_gra.Properties.Resources.trollface;
+            this.pictureBox1.Location = new System.Drawing.Point(176, 173);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(120, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Liczba błędów:";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(320, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 33);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(-10, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(509, 111);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Witaj w grze na refleks! \r\nWciśnij \'Start\', aby rozpocząć grę. \r\nPowodzenia!";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(214, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 64);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(326, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 51);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Wyjdź";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(471, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gra na refleks";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ losowa gra/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace losowa_gra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            label5.Location = new Point(this.Width / 2 - 250, this.Height / 2 - 390);
+            button1.Size = new System.Drawing.Size(250, 80);
+            button1.Location = new Point(this.Width / 2 - 200, this.Height/2 - 250);
+            button2.Size = new System.Drawing.Size(250, 80);
+            button2.Location = new Point(this.Width / 2 - 200, this.Height / 2 - 140);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random miejsce = new Random();
+            Point punkt = new Point();
+
+            int x = miejsce.Next(50, this.Width - 150);
+            int y = miejsce.Next(100, this.Height - 150);
+
+            punkt = new Point(x, y);
+
+            pictureBox1.Location = punkt;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Random miejsce = new Random();
+            Point punkt = new Point();
+
+            int x = miejsce.Next(50, this.Width - 150);
+            int y = miejsce.Next(100, this.Height - 150);
+
+            punkt = new Point(x, y);
+
+            pictureBox1.Location = punkt;
+
+            int licznik = 1;
+            int wynik = licznik + Convert.ToInt32(label2.Text);
+            if ((wynik % 5 == 0) && (timer1.Interval >= 600))
+            {
+                timer1.Interval = timer1.Interval - 200;
+            }
+            label2.Text = Convert.ToString(wynik);
+
+            timer1.Stop();
+            timer1.Start();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = MousePosition.X;
+            int y = MousePosition.Y;
+
+            if ((x < pictureBox1.Left) || (x > (pictureBox1.Left + pictureBox1.Width))
+              || (y < pictureBox1.Top) || (y > (pictureBox1.Top + pictureBox1.Height)))
+            {
+                int licznik = 1;
+                int wynik = licznik + Convert.ToInt32(label4.Text);
+                label4.Text = Convert.ToString(wynik);
+
+                if (wynik >= 10)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Przegrana! Osiągnąłeś wynik: " + label2.Text);
+
+                    pictureBox1.Visible = false;
+                    label1.Visible = false;
+                    label2.Visible = false;
+                    label2.Text = "0";
+                    label3.Visible = false;
+                    label4.Visible = false;
+                    label4.Text = "0";
+
+                    label5.Text = "Jeszcze raz?";
+                    label5.Visible = true;
+                    button1.Visible = true;
+                    button2.Visible = true;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            label5.Visible = false;
+
+            pictureBox1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+
+            pictureBox1.Location = new Point(this.Width / 2 - 80, this.Height / 2 - 100);
+            label1.Location = new Point(this.Width / 2 - 390);
+            label2.Location = new Point(this.Width / 2 - 220);
+            label3.Location = new Point(this.Width / 2 + 150);
+            label4.Location = new Point(this.Width / 2 + 350);
+
+            timer1.Interval = 1200;
+            timer1.Enabled = true;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
